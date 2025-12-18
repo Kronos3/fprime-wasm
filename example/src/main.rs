15af22e24bfd5e0784aad9d551ba7c29f3ab458f
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2025-12-18 15:30:24 -0800

    Cleanup

--- example/src/main.rs
+++ example/src/main.rs
@@ -5,13 +5,9 @@ mod RefTopologyDictionary;
 
 pub use RefTopologyDictionary::*;
 
-fn entry() {
-    cdh_core::cmd_disp::cmd_no_op();
-    cdh_core::cmd_disp::cmd_no_op();
-    cdh_core::cmd_disp::cmd_no_op();
-}
-
 #[unsafe(no_mangle)]
 pub extern "C" fn main() {
-    entry();
+    cdh_core::cmd_disp::cmd_no_op();
+    cdh_core::cmd_disp::cmd_no_op();
+    cdh_core::cmd_disp::cmd_no_op();
 }
